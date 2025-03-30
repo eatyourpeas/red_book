044e8e4b88a28006c3b5fc64f0f1654c45dbdb47
eatyourpeas <simon@eatyourpeas.co.uk>
2025-03-30 16:06:24 +0100

    adds logo and redbook red

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
+import 'dart:ui';
+import 'package:google_fonts/google_fonts.dart';
+
+const redBookRed = Color.fromARGB(255, 237, 29, 36);
 
 void main() {
-  runApp(const MyApp());
+  runApp(const RedBookApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class RedBookApp extends StatelessWidget {
+  const RedBookApp({super.key});
 
   // This widget is the root of your application.
   @override
@@ -13,30 +17,15 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: redBookRed),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const RedBookHome(title: 'Red Book'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class RedBookHome extends StatefulWidget {
+  const RedBookHome({super.key, required this.title});
 
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
@@ -50,10 +39,10 @@ class MyHomePage extends StatefulWidget {
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<RedBookHome> createState() => _RedBookHomeState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _RedBookHomeState extends State<RedBookHome> {
   int _counter = 0;
 
   void _incrementCounter() {
@@ -85,31 +74,44 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+      body: Container( // Directly use Container as the body
+        color: redBookRed, // Set the background color here
+        child: Center( // Reintroduce Center to center content
+          child: Padding( // Optional padding
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+            Padding(
+            padding: const EdgeInsets.only(bottom: 20),
+            child: // Add padding below the text
+                Align(
+                  alignment: Alignment.center,
+                  child: Text(
+                    "My Personal Child Health Record",
+                    textAlign: TextAlign.left, // Keep text left-aligned internally
+                    style: GoogleFonts.lexend( // Apply the Lato font here
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white
+                    ),
+                  ),
+                ),
+        ),
+
+                Image.asset(
+                  'assets/redbook.png',
+                  height: 200, // Adjust height as needed
+                ),
+                const SizedBox(height: 20), // Add spacing between image and text
+                const Text('You have pushed the button this many times:'),
+                Text(
+                  '$_counter',
+                  style: Theme.of(context).textTheme.headlineMedium,
+                ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
